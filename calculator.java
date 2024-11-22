9c13b510535c7fdce1779fb0b7ca2eac44cb3f88
swaraj-shubh <shubhh.ab@gmail.com>
2024-11-23 01:17:27 +0530

    Update calculator.java

---
+++ calculator.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class calculator{
+    public static void main(Scanner args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no.: ");
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
